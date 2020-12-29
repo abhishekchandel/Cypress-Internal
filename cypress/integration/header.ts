e99d8460609e3582ba199cf009c4c63e877d84b3
Deepak <deepaksharmawins@gmail.com>
2020-12-29 16:50:04 +0530

    click account option and sign out

--- cypress/integration/header.ts
+++ cypress/integration/header.ts
@@ -1,11 +1,11 @@
 /// <reference types="Cypress" />
-describe('Header Tests', function() {
+describe('Header Tests', function () {
     // we can use these values to log in
-  const username = 'manasbiswaswins@gmail.com'
-  const password = 'Techie@1234'
+    const username = 'manasbiswaswins@gmail.com'
+    const password = 'Techie@1234'
 
     beforeEach(() => {
-        cy.loginByForm(username,password);
+        cy.loginByForm(username, password);
     });
 
     it('should click icons on header', () => {
@@ -28,11 +28,51 @@ describe('Header Tests', function() {
             .click()
         cy.wait(2000);
 
+        //click on settings icon to show settings pop up
+        cy.get('#settingsPopup')
+            .should('exist').should('be.visible')
+            .click()
+        cy.wait(2000);
+
+
         //click on account drive app suite ddlb
         cy.get('#navPopup')
             .should('exist').should('be.visible')
             .click()
         cy.wait(8000);
+
+        //click on Account in account drive app suite ddlb
+        cy.get('[role="option"]').contains('Account')
+            .should('exist').should('be.visible')
+            .click()
+             cy.wait(2000);
+
+
+        // //click on account drive app suite ddlb
+        // cy.get('#navPopup')
+        //     .should('exist').should('be.visible')
+        //     .click()
+        // cy.wait(8000);
+
+        // //click on location in account drive app suite ddlb
+        // cy.get('[role="option"]').contains('Location')
+        //     .should('exist').should('be.visible')
+        //     .click()
+        // cy.wait(3000);
+
+        // //click on MSO in account drive app suite ddlb
+        // cy.get('.d-nav-item dx-template-wrapper dx-item-content dx-list-item-content').contains('MSO')
+        //     .should('exist').should('be.visible')
+        //     .click()
+        // cy.wait(1000);
+
+
+
+
+
+
+
+
 
         //click on settings icon to show settings pop up
         cy.get('#settingsPopup')
@@ -48,7 +88,7 @@ describe('Header Tests', function() {
         cy.get('.dx-toolbar-center').contains('Yes')
             .should('exist').should('be.visible')
             .click()
-    
+
     });
-    
-    });  
+
+});  
